05b5c11870634a1d4afc351a85f7556f4ae5d677
Sridharrr <sridharnarayananit@gmail.com>
2017-06-08 10:58:15 +0530

    Create Duplicate.java

---
+++ Duplicate.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+ class Duplicate
+ { 
+	public static void main(String args[])
+ 	{
+ 	    Scanner sr=new Scanner(System.in);
+ 		int i;int n=sr.nextInt();
+ 		int []a=new int[n];int c=0;
+ 		for(i=0;i<n;i++)
+ 		{
+ 		    a[i]=sr.nextInt();
+ 		}
+ 		Set<Integer> s=new HashSet<Integer>();
+ 		for(i=0;i<n;i++)
+ 		{
+ 		    if(s.add(a[i])==false)
+ 		    {
+ 		      c=c+1;
+ 		       System.out.println(a[i]);
+ 		    }
+ 		}
+ 		if(c<=0)
+ 		{
+ 		     System.out.println("No repeated values found");
+ 		}
+ 	
+ 	}
+ }
