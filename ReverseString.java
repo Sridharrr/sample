d8274d2a54c017b402404c38da069c6a895fc0a5
Sridharrr <sridharnarayananit@gmail.com>
2017-06-09 12:19:34 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class TimeDiffer {
+    public static void main(String args[])
+    {
+        Scanner sr=new Scanner(System.in);
+   String s=sr.nextLine();
+   StringBuffer s1=new StringBuffer(s);
+   s1.reverse();
+   System.out.println(s1);
+        }
+          }
+
+       
