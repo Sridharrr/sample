adb577bfff389df926a93c0da423fde2b5324563
Sridharrr <sridharnarayananit@gmail.com>
2017-06-12 11:16:22 +0530

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class Simple {
+    public static void main(String args[]){
+   Scanner sr=new Scanner(System.in);
+        int n=sr.nextInt();
+        float p=sr.nextFloat();
+        float r=sr.nextFloat();
+        System.out.print((p*n*r)/100);
+	}
+}
